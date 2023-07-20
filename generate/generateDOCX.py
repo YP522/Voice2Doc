f4c480fc7630e9e61cf0eb3ef69f67c4508408f3
YP522 <yoannpetit22@free.fr>
2023-07-20 23:12:53 +0200

    ✨ feat: Word+UnknownValErr

--- generate/generateDOCX.py
+++ generate/generateDOCX.py
@@ -1,7 +1,42 @@
 import docx
+import os
+# from docx import Document
 from system import utils as u
 
 def makeDoc(name,txt):
     doc = docx.Document()
     doc.add_paragraph(txt)
-    doc.save(u.dt_string+"/docs_chunks/"f'{name}.docx')    
+    doc.save(u.dt_string+"/docs_chunks/"f'{name}.docx')
+
+def get_docx_files(path):
+    files = []
+    for file in os.listdir(path):
+        if file.endswith(".docx"):
+            files.append(os.path.join(path, file))
+    return files
+
+def get_docx_text(file):
+    doc = docx.Document(file)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+
+
+def combine_word_documents(files):
+    merged_document = docx.Document()
+
+    for index, file in enumerate(files):
+        sub_doc = docx.Document(file)
+
+        # Don't add a page break if you've reached the last file.
+        if index < len(files)-1:
+           sub_doc.add_page_break()
+
+        for element in sub_doc.element.body:
+            merged_document.element.body.append(element)
+
+    merged_document.save(u.dt_string+'/merged.docx')
+
+  
--- processing/extract.py
+++ processing/extract.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
 import os
+from system import utils as u
 
-from generate.generateDOCX import makeDoc
+from generate.generateDOCX import combine_word_documents, get_docx_files, makeDoc
+from tqdm import tqdm
 
 def fetchAllWav(path):
     """
@@ -10,27 +12,31 @@ def fetchAllWav(path):
     
     :param path: The path to the folder containing the wav files
     """
-
     for root, dirs, files in os.walk(path):
-        for filename in files:
-    
-            extract_from(path+"/"+filename)
+        for filename in tqdm(files):
+            extract_from(os.path.join(path, filename))
+
+import os
+import speech_recognition as sr
 
 def extract_from(source):
     """
-    It takes a file name as input, extracts the text from the file, and then writes the text to a new
-    file
+    Prend en entrée le nom d'un fichier, extrait le texte du fichier, puis écrit le texte dans un nouveau fichier
     
-    :param source: the path to the audio file
+    :param source: le chemin vers le fichier audio
     """
-    r=sr.Recognizer()
-
+    r = sr.Recognizer()
     basename_without_ext = os.path.splitext(os.path.basename(source))[0]
-
-    harvad=sr.AudioFile(source)
+    harvad = sr.AudioFile(source)
 
     with harvad as source:
-        audio=r.record(source)
+        audio = r.record(source)
 
-    val=r.recognize_google(audio, language="fr-FR")
-    makeDoc(basename_without_ext,val)
+    try:
+        val = r.recognize_google(audio, language="fr-FR")
+    except sr.UnknownValueError:
+        val = "[mot ou phrase non reconnue]"
+    
+    makeDoc(basename_without_ext, val)
+    files = get_docx_files(u.dt_string+"/docs_chunks")
+    combine_word_documents(files)
